ddbc8920f9b18557a3ef4452050295030bbbe95a
kriskoin <cris7775@gmail.com>
2024-10-09 23:57:08 -0600

    add: lecture 5

--- course1/section1/hello.rs
+++ course1/section1/hello.rs
@@ -1,3 +1,11 @@
 fn main (){
-    println!("{ }",3.5);
+    let s = "hello using letksks";
+    println!("esto '{ }' es un sting",s);
+
+    let an_integer:i32=-5;
+    println!("esto es un entero { }",an_integer);
+
+    let an_float:f32=3.14;
+    println!("esto es {} un float",an_float);
+    
 }
