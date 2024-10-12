c1ca98674dc03905b9dd70fecd7914c73419e906
Kriskoin <cris7775@gmail.com>
2024-10-12 07:39:08 -0600

    add: lecture 10

---
+++ course1/section1/shadowing.rs
@@ -0,0 +1,18 @@
+fn main(){
+    let _an_32_integer:i32=5;
+    let _an_64_integer:i64=37;
+    let _an_32_float:f32=1.5;
+    let _an_64_float:f64=31.5;
+    let _another_64_float:f64=11.5;
+
+    let a = 10;
+    println!("{}",a);
+    let a = 20;
+    println!("{}",a);
+
+    let an_variable:i32=5;
+    println!("an_variable as integer {}",an_variable);
+
+    let an_variable:char='c';
+    println!("an_variable as  char {}",an_variable);
+}
