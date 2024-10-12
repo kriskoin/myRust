68b51f2768938057959019921700080aef993c3e
Kriskoin <cris7775@gmail.com>
2024-10-12 07:58:02 -0600

    add: more shadowing examples

--- course1/section1/shadowing.rs
+++ course1/section1/shadowing.rs
@@ -15,4 +15,33 @@ fn main(){
 
     let an_variable:char='c';
     println!("an_variable as  char {}",an_variable);
+
+
+ 
+    let precio = "10";
+    let precio: i32 = precio.parse().expect("No es un número");
+    let precio_con_iva = precio + (precio * 21 / 100);
+    println!("El precio con IVA es: {}", precio_con_iva);
+
+
+    
+    let x = 10;
+    println!("Fuera del ámbito, x = {}", x); // x vuelve a ser 10
+    {
+        let x = x * 2; // x dentro del ámbito es 20
+        println!("Dentro del ámbito, x = {}", x);
+
+        let x = 'C'; // x dentro del ámbito es C
+        println!("Dentro del ámbito ahora soy un caracter, x = {}", x);
+        {
+            let x = 3.1454; // x dentro del ámbito es 20
+            println!("Dentro del sub ámbito, ahora soy 3.14 x = {}", x);
+        }
+        println!("Afuera del sub ámbito, x = {}", x);
+    }
+    println!("Fuera del ámbito, x = {}", x); // x vuelve a ser 10
+
+    
+    
+    
 }
