2e2d0c199f0889903a32bd4af9f29a5693843b79
Kriskoin <cris7775@gmail.com>
2024-10-27 23:54:34 -0600

    add: Lecture 30

---
+++ course1/section4/fact.rs
@@ -0,0 +1,13 @@
+fn main (){
+    let x  = fact(5);
+    println! ("{:?}",x);
+}
+
+fn fact (a:i32)-> (String,i32){
+    
+    let mut fact =1;
+    for n in 1 .. a+1 {
+        fact = fact * n;
+    }
+    (String::from("Factorial of:"),fact)
+}
